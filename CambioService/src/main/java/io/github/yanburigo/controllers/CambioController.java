ea87d8efeb022c5fb6c86eede6f033ca58fa31be
Yan Burigo <yanburigoribeiro@gmail.com>
2023-11-18 12:09:08 -0300

    Create integration with database

--- CambioService/src/main/java/io/github/yanburigo/controllers/CambioController.java
+++ CambioService/src/main/java/io/github/yanburigo/controllers/CambioController.java
@@ -1,6 +1,7 @@
 package io.github.yanburigo.controllers;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.yanburigo.modal.Cambio;
+import io.github.yanburigo.repository.CambioRepository;
 
 @RestController
 @RequestMapping("cambio-service")
@@ -18,9 +20,20 @@ public class CambioController {
 	@Autowired
 	private Environment environment;
 	
+	@Autowired
+	private CambioRepository repository;
+	
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
+		var cambio = repository.findByFromAndTo(from, to);
+		if(cambio == null) throw new RuntimeException("Currency Unsupported");
+		
 		var port = environment.getProperty("local.server.port");
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+		BigDecimal conversionFactor = cambio.getConversionFactor();
+		BigDecimal convertedValue = conversionFactor.multiply(amount);
+		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+		cambio.setEnviroment(port);
+		
+		return cambio;
 	}
 }
