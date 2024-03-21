53b9cd62782dbf4cea27a8a2dbb4e494c3861694
Yan Burigo <yanburigoribeiro@gmail.com>
2024-03-21 10:29:34 -0300

    Create docker containers and fix gateway errors

--- CambioService/src/main/java/io/github/yanburigo/controllers/CambioController.java
+++ CambioService/src/main/java/io/github/yanburigo/controllers/CambioController.java
@@ -3,6 +3,8 @@ package io.github.yanburigo.controllers;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("cambio-service")
 public class CambioController {
 	
+	private Logger logger = LoggerFactory.getLogger(CambioController.class);
+	
 	@Autowired
 	private Environment environment;
 	
@@ -29,6 +33,7 @@ public class CambioController {
 	@Operation(description = "Get cambio from currency")
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
+		logger.info("getCambio is called with - {}, {} and {}", amount, from, to);
 		var cambio = repository.findByFromAndTo(from, to);
 		if(cambio == null) throw new RuntimeException("Currency Unsupported");
 		
